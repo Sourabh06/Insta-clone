c9a72d107d493363696672febe96c79a612954d3
Sourabh06 <sourabh6sept@gmail.com>
2020-07-03 21:35:29 +0530

    fix typo

--- app.js
+++ app.js
@@ -31,6 +31,10 @@ if(process.env.NODE_ENV == "production"){
     app.get('*',(req,res) => {
         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
     })
+    // app.get('*', (req, res) => {
+    //     const index = path.join(__dirname, 'client','build', 'index.html');
+    //     res.sendFile(index);
+    // });
 }
 
 app.listen(PORT, () => {
